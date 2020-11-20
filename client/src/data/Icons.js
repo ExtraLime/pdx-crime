60c5d7b999c96cedb3423788a50633c381f9ccce
ExtraLime <willdox7@live.com>
2020-11-19 18:53:47 -0800

    pushing env to heroku, added point to static assets in production

--- client/src/data/Icons.js
+++ client/src/data/Icons.js
@@ -1,14 +1,14 @@
 import { Icon } from "leaflet";
 
 export const medical = new Icon({
-  iconUrl: "static/hospital.svg",
+  iconUrl: "https://symbols.getvecta.com/stencil_214/17_ambulance.d82dadcb18.svg",
   iconSize: [25, 25]
 });
 export const police = new Icon({
-  iconUrl: "static/police.svg",
+  iconUrl: "https://symbols.getvecta.com/stencil_34/22_male-police-officer.983b34e416.svg",
   iconSize: [25, 25]
 });
 export const fireman = new Icon({
-  iconUrl: "static/fireman.svg",
+  iconUrl: "https://symbols.getvecta.com/stencil_211/32_fire.442c3728d6.svg",
   iconSize: [25, 25]
 });
